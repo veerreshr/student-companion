e68f20c177f76b5cbac7796862809fe474f0ffc6
veeresh <veerreshr@gmail.com>
2020-04-14 05:27:37 +0530

    remarks styling done , timetable logic done

--- index.php
+++ index.php
@@ -1,39 +1,42 @@
 <?php
 session_start();
 require './db.php';
-if(isset($_GET['present'])){
-    $weekid=$_GET['weekid'];
-   $present=$_GET['present'];
-    $date=$_GET['date'];
-    $query="select * from daily where weekid=$weekid and date=$date and id=".$_SESSION['id']."";
+if (isset($_GET['present'])) {
+    $weekid = $_GET['weekid'];
+    $present = $_GET['present'];
+    $date = $_GET['date'];
+    $query = "select * from daily where weekid=$weekid and date=$date and id=" . $_SESSION['id'] . "";
     $result = mysqli_query($db, $query);
-    $rowcount = mysqli_num_rows($result);
-    if($rowcount==0){
-        $query="insert into daily (id,date,weekid,present,holiday) values(".$_SESSION['id'].",'$date',$weekid,$present,0)";
+
+    $user = mysqli_fetch_assoc($result);
+    if ($user['present'] == 1 || $user['present'] == 0) {
+        $query = "update daily set present=$present ,holiday=0 where id=" . $_SESSION['id'] . " and weekid=$weekid and date='$date' ";
         mysqli_query($db, $query) or die(mysqli_error($db));
-    }else{
-        $query="update daily set present=$present where id=".$_SESSION['id']." and weekid=$weekid and date='$date' ";
+    } else {
+
+        $query = "insert into daily (id,date,weekid,present,holiday) values(" . $_SESSION['id'] . ",'$date',$weekid,$present,0)";
         mysqli_query($db, $query) or die(mysqli_error($db));
     }
 }
-if(isset($_GET['holiday'])){
-    $weekid=$_GET['weekid']; 
-    $date=$_GET['date'];
-    $holiday=$_GET['holiday'];
-    $query="select * from daily where weekid=$weekid and date=$date and id=".$_SESSION['id']."";
+if (isset($_GET['holiday'])) {
+    $weekid = $_GET['weekid'];
+    $date = $_GET['date'];
+    $holiday = $_GET['holiday'];
+    $query = "select * from daily where weekid=$weekid and date=$date and id=" . $_SESSION['id'] . "";
     $result = mysqli_query($db, $query);
-    $rowcount = mysqli_num_rows($result);
-    if($rowcount==0){
-        $query="insert into daily (id,date,weekid,holiday) values(".$_SESSION['id'].",'$date',$weekid,$holiday)";
+
+    $user = mysqli_fetch_assoc($result);
+    if ($user['holiday'] == 1 || $user['present'] == 1 || $user['present'] == 0) {
+        $query = "update daily set holiday=$holiday ,present=NULL where id=" . $_SESSION['id'] . " and weekid=$weekid and date='$date' ";
         mysqli_query($db, $query) or die(mysqli_error($db));
-    }else{
-        $query="update daily set holiday=$holiday ,present=NULL where id=".$_SESSION['id']." and weekid=$weekid and date='$date' ";
+    } else {
+        $query = "insert into daily (id,date,weekid,holiday) values(" . $_SESSION['id'] . ",'$date',$weekid,$holiday)";
         mysqli_query($db, $query) or die(mysqli_error($db));
     }
 }
 require 'subject.php';
 
-$now = time() ; //for indian timestamp 
+$now = time(); //for indian timestamp 
 
 if (!isset($_SESSION['name'])) {
     $_SESSION['msg'] = "You must log in first";
@@ -56,7 +59,7 @@ if (isset($_GET['logout'])) {
 }
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" >
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html lang="en">
 
 <head>
@@ -64,13 +67,11 @@ if (isset($_GET['logout'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student companion </title>
     <link rel="stylesheet" type="text/css" href="style1.css">
-    <script>
-        
 
-    </script>
+
 </head>
 
-<body onload="start()" onresize="start()" >
+<body onload="start()" onresize="start()">
     <div class="header">
         <div class="navbar" id="navbar">
             <div class="menu" id="hamburger">
@@ -91,18 +92,18 @@ if (isset($_GET['logout'])) {
         <div class="tab">
             <button class="tablinks" onclick="shuffle(event, 'timetable')" id="defaultOpen">Timetable</button>
             <button class="tablinks" onclick="shuffle(event, 'todo')">Take note</button>
-            <button class="tablinks" onclick="shuffle(event, 'remarks')">Remark</button>
+            <button class="tablinks" id="remarkslink" onclick="shuffle(event, 'remarks')">Remark</button>
         </div>
         <div id="timetable" class="tabcontent">
 
             <div class="row1">
-                
+
                 <?php
-                 $query="select max(lastupdate)as last from week where id='".$_SESSION['id']."'";
-                 $result = mysqli_query($db, $query);
-                 $latest=mysqli_fetch_assoc($result);
-                 $from=date("Y-m-d H:i:s",(strtotime($latest['last'])-(15*60)));
-                 $to=date("Y-m-d H:i:s",(strtotime($latest['last'])+(15*60)));
+                $query = "select max(lastupdate)as last from week where id='" . $_SESSION['id'] . "'";
+                $result = mysqli_query($db, $query);
+                $latest = mysqli_fetch_assoc($result);
+                $from = date("Y-m-d H:i:s", (strtotime($latest['last']) - (15 * 60)));
+                $to = date("Y-m-d H:i:s", (strtotime($latest['last']) + (15 * 60)));
                 $query = "select min(stime) as mini from week where id='" . $_SESSION['id'] . "' and lastupdate between '$from' and '$to'";
                 $result = mysqli_query($db, $query);
                 $user = mysqli_fetch_assoc($result);
@@ -119,13 +120,13 @@ if (isset($_GET['logout'])) {
                 <?php
                 for ($i = 0; $i < $twh; $i++) {
                 ?>
-                    <div class="time" ><?php echo gmdate("h:i a", $starttime) . "<br>-<br>" . gmdate("h:i a", $starttime + 3600); ?></div>
+                    <div class="time"><?php echo gmdate("h:i a", $starttime) . "<br>-<br>" . gmdate("h:i a", $starttime + 3600); ?></div>
                 <?php
                     $starttime = $starttime + 3600;
                 }
                 ?>
             </div>
-                <?php subject($now); ?>
+            <?php subject($now); ?>
 
         </div>
         <div id="todo" class="tabcontent">
@@ -151,100 +152,145 @@ if (isset($_GET['logout'])) {
 
         </div>
         <div id="remarks" class="tabcontent">
-            <center>student remarks in all subjects</center>
+            <div class="outerbox">
+                <div class="card">
+                    <div class="graph">
+                    <div class="chart" data-percent="73" data-scale-color="#ffb400"><p>73%</p></div>
+                    </div>
+                    <div class="description">
+                        <h2>Classic Peace Lily</h2>
+                        <h4>Popular House Plant</h4>
+                        <h1>$18</h1>
+                        <p>Classic Peace Lily is a spathiphyllum floor plant arranged in a bamboo planter with a blue & red ribbom and butterfly pick.</p>
+                        <button>calendar</button>
+
+                    </div>
+                </div>
+            </div>
+            <div class="outerbox">
+                <div class="card">
+                    <div class="graph">
+                    <div class="chart" data-percent="60" data-scale-color="#ffb400" data-bar-color="blue"><p>73%</p></div>
+                    </div>
+                    <div class="description">
+                        <h2>Classic Peace Lily</h2>
+                        <h4>Popular House Plant</h4>
+                        <h1>$18</h1>
+                        <p>Classic Peace Lily is a spathiphyllum floor plant arranged in a bamboo planter with a blue & red ribbom and butterfly pick.</p>
+                        <button>calendar</button>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
     <div class="footer"></div>
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="jquery.easypiechart.js"></script>
     <script>
+    $("#remarkslink").click(function() {
+        $('.chart').easyPieChart({
+            scaleLength:8,
+            size:200,
+            lineWidth:6,
+        });
+    });
+</script>
+
+  <script>
+    
         // for automating the sizes of subjects
         var oneunit;
-                function autosize(){
-                        var y = document.getElementsByClassName('time');
-                        oneunit=y[0].offsetWidth;
-                       
-                        var x = document.getElementsByClassName('firstcol');
-                        var i;
-                        for (i = 0; i < 7; i++){
-                        x[i].style.width = oneunit+'px';
-                        }
-                var z= document.getElementsByClassName('subject');
-                for (i = 0; i < z.length; i++){
-                    var end=getseconds(z[i].getAttribute('data-etime'));
-                    var start=getseconds(z[i].getAttribute('data-stime'));
-                    console.log(((end-start)/3600)*oneunit);
-                    var size=((end-start)/3600)*oneunit;
-                    z[i].style.width=size+"px";
 
-                }
-                }
+        function autosize() {
+            var y = document.getElementsByClassName('time');
+            oneunit = y[0].offsetWidth;
+
+            var x = document.getElementsByClassName('firstcol');
+            var i;
+            for (i = 0; i < 7; i++) {
+                x[i].style.width = oneunit + 'px';
+            }
+            var z = document.getElementsByClassName('subject');
+            for (i = 0; i < z.length; i++) {
+                var end = getseconds(z[i].getAttribute('data-etime'));
+                var start = getseconds(z[i].getAttribute('data-stime'));
+                console.log(((end - start) / 3600) * oneunit);
+                var size = ((end - start) / 3600) * oneunit;
+                z[i].style.width = size + "px";
+
+            }
+        }
 
         // end of automating sizes
-      
-            
-            
-           
-            //adding free classes to everyday
-            function addfree(day){
-               var startsAt=document.getElementById('startendtime').getAttribute("start");
-               var endsAt=document.getElementById('startendtime').getAttribute("end");
-               
-                var x = document.getElementById(day).childElementCount; // length of child elements of particular day
-              
-                var previoustime=getseconds(startsAt);
-                
-                var endtime=getseconds(endsAt);
-                console.log(day);
-                var list = document.getElementById(day);
-                for (let i = 1; i < x; i++) {
-                   
-                    
-                    var childstime=getseconds(list.children[i].getAttribute('data-stime'));
-                    var childetime=getseconds(list.children[i].getAttribute('data-etime'));
-                     if( childstime!=previoustime){
-                         var timegap=(childstime-previoustime)/3600; //in hours
-                         var childwidth=oneunit*timegap;
-                         var newItem = document.createElement("div");  
-                         newItem.className="free";     
-                         newItem.style.width=childwidth+"px";
-                         var textnode = document.createTextNode(" ");  
-                         newItem.appendChild(textnode);                   
-                         list.insertBefore(newItem, list.children[i]);
-                         i++;
-                         x++;
-                     }
-                     previoustime=childetime;
-                    
+
+
+
+
+        //adding free classes to everyday
+        function addfree(day) {
+            var startsAt = document.getElementById('startendtime').getAttribute("start");
+            var endsAt = document.getElementById('startendtime').getAttribute("end");
+
+            var x = document.getElementById(day).childElementCount; // length of child elements of particular day
+
+            var previoustime = getseconds(startsAt);
+
+            var endtime = getseconds(endsAt);
+            console.log(day);
+            var list = document.getElementById(day);
+            for (let i = 1; i < x; i++) {
+
+
+                var childstime = getseconds(list.children[i].getAttribute('data-stime'));
+                var childetime = getseconds(list.children[i].getAttribute('data-etime'));
+                if (childstime != previoustime) {
+                    var timegap = (childstime - previoustime) / 3600; //in hours
+                    var childwidth = oneunit * timegap;
+                    var newItem = document.createElement("div");
+                    newItem.className = "free";
+                    newItem.style.width = childwidth + "px";
+                    var textnode = document.createTextNode(" ");
+                    newItem.appendChild(textnode);
+                    list.insertBefore(newItem, list.children[i]);
+                    i++;
+                    x++;
                 }
-            if(previoustime!=endtime){
-                var timegap=(endtime-previoustime)/3600; //in hours
-                var childwidth=oneunit*timegap;
-                var newItem = document.createElement("div");  
-                newItem.className="free";     
-                newItem.style.width=childwidth+"px";
-                var textnode = document.createTextNode(" ");  
-                newItem.appendChild(textnode);                   
+                previoustime = childetime;
+
+            }
+            if (previoustime != endtime) {
+                var timegap = (endtime - previoustime) / 3600; //in hours
+                var childwidth = oneunit * timegap;
+                var newItem = document.createElement("div");
+                newItem.className = "free";
+                newItem.style.width = childwidth + "px";
+                var textnode = document.createTextNode(" ");
+                newItem.appendChild(textnode);
                 document.getElementById(day).appendChild(newItem);
             }
-           
-            }
-            
-            //end of everyday
-            function getseconds(time){
-                var a = time.split(':'); // split it at the colons
-                var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 ; 
-                return seconds;
-            }
-            function start(){
-                autosize();
-                addfree("Mon");
-                addfree("Tue");
-                addfree("Wed");
-                addfree("Thu");
-                addfree("Fri");
-                addfree("Sat");
-                addfree("Sun");
-            }
-                
+
+        }
+
+        //end of everyday
+        function getseconds(time) {
+            var a = time.split(':'); // split it at the colons
+            var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60;
+            return seconds;
+        }
+
+        function start() {
+            autosize();
+            addfree("Mon");
+            addfree("Tue");
+            addfree("Wed");
+            addfree("Thu");
+            addfree("Fri");
+            addfree("Sat");
+            addfree("Sun");
+        }
+
         //end of free classes
 
         //for nav tags
@@ -333,7 +379,8 @@ if (isset($_GET['logout'])) {
 
         //end for todo
     </script>
-   
+
+
 </body>
 
 </html>
--- subject.php
+++ subject.php
@@ -3,10 +3,10 @@
             
             function subject($time){
                 $daylist=array('Mon','Tue','Wed','Thu','Fri','Sat','Sun');
-                $presentday = date('D', $time);
+                $presentday = date('D', $time+19800);
                 switch($presentday){
                     case 'Mon' :  
-                                    for($i=0;$i<6;$i++){
+                                    for($i=0;$i<7;$i++){
                                         row($time+($i*86400),$daylist[$i],$time);
                                     }                
                                     break;
@@ -81,12 +81,12 @@
                 $latest=mysqli_fetch_assoc($result);
                 $from=date("Y-m-d H:i:s",(strtotime($latest['last'])-(15*60)));
                 $to=date("Y-m-d H:i:s",(strtotime($latest['last'])+(15*60)));
-                $query="select * from week where day='$day' and id='".$_SESSION['id']."' and lastupdate between '$from' and '$to'";
+                $query="select * from week where day='$day' and id='".$_SESSION['id']."' and lastupdate between '$from' and '$to' order by stime asc";
                 $result = mysqli_query($db, $query) or die(mysqli_error($db)); 
 ?> 
  
             <div id="<?php echo $day; ?>">
-            <div class="firstcol"><?php echo $day ?></div>
+            <div class="firstcol" <?php if($time==$today)echo 'today'?>>    <?php echo $day ?></div>
                 <?php
                 while($row = mysqli_fetch_assoc($result)){
             $weekid=$row['weekid'];
@@ -95,28 +95,23 @@
             $etime=gmdate("H:i",strtotime($row['etime']));
             $dtime=strtotime($etime)-strtotime($stime);
             $query="select * from daily where id=".$_SESSION['id']." and weekid=$weekid and date='".date("Y-m-d ",$time+19800)."'";
-            $result = mysqli_query($db, $query) or die(mysqli_error($db));
-            $rowcount = mysqli_num_rows($result);
-            
-            ?>
-            <div class="subject <?php if($time==$today)echo 'today ';
-            if($rowcount!=0){
-                $row= mysqli_fetch_assoc($result);
-                if($row['holiday']==1){
-                    echo "holiday";
-                }elseif($row['present']==1){
-                    echo "present";
-                }elseif($row['present']==0){
-                    echo "absent";
-                }
+            $result2 = mysqli_query($db, $query) or die(mysqli_error($db));
+            $classname='subject';
+            if(mysqli_num_rows($result2)!=0){
+                $row2= mysqli_fetch_assoc($result2);
+                if($row2['present']==0){$classname='absent';}
+                if($row2['present']==1){$classname='present';}
+                if($row2['holiday']==1){$classname='holiday';}
             }
-            
-            ?>" data-stime="<?php echo $stime;  ?>" data-etime="<?php echo $etime;  ?>" data-weekid="<?php echo $weekid; ?>">
+           
+
+            ?>
+            <div class="subject <?php echo $classname; ?>" data-stime="<?php echo $stime;  ?>" data-etime="<?php echo $etime;  ?>" data-weekid="<?php echo $weekid; ?>">
             <div class="inner"><?php  echo $subject; ?></div>
             <div class="links">
-                <a title="present" href="<?php if($time<$today)echo "index.php?weekid=$weekid"."&date=".date("Y-m-d ",$time+19800)."&present=1";   ?>"><svg class="yes" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4 8-8z"/></svg></a>
-                <a title="absent" href="<?php if($time<$today)echo "index.php?weekid=$weekid"."&date=".date("Y-m-d",$time+19800)."&present=0";   ?>"><svg class="no" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg></a>
-                <a title="holiday" href="<?php if($time<$today)echo "index.php?weekid=$weekid"."&date=".date("Y-m-d",$time+19800)."&holiday=1";   ?>"><svg class="hday" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 11h2v2H7v-2zm14-5v14c0 1.1-.9 2-2 2H5c-1.11 0-2-.9-2-2l.01-14c0-1.1.88-2 1.99-2h1V2h2v2h8V2h2v2h1c1.1 0 2 .9 2 2zM5 8h14V6H5v2zm14 12V10H5v10h14zm-4-7h2v-2h-2v2zm-4 0h2v-2h-2v2z"/></svg></a>
+                <a title="present" href="<?php if($time<=$today)echo "index.php?weekid=$weekid"."&date=".date("Y-m-d ",$time+19800)."&present=1";   ?>"><svg class="yes" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4 8-8z"/></svg></a>
+                <a title="absent" href="<?php if($time<=$today)echo "index.php?weekid=$weekid"."&date=".date("Y-m-d",$time+19800)."&present=0";   ?>"><svg class="no" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg></a>
+                <a title="holiday" href="<?php if($time<=$today)echo "index.php?weekid=$weekid"."&date=".date("Y-m-d",$time+19800)."&holiday=1";   ?>"><svg class="hday" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 11h2v2H7v-2zm14-5v14c0 1.1-.9 2-2 2H5c-1.11 0-2-.9-2-2l.01-14c0-1.1.88-2 1.99-2h1V2h2v2h8V2h2v2h1c1.1 0 2 .9 2 2zM5 8h14V6H5v2zm14 12V10H5v10h14zm-4-7h2v-2h-2v2zm-4 0h2v-2h-2v2z"/></svg></a>
             </div>
             </div>
             <?php
